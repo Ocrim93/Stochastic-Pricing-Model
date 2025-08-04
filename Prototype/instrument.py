3410a415c543cedda47f03cda9d9f967e88447b4
mirco <mirco@MacBook-Air.local>
2025-08-04 10:58:25 +0100

    feat : volatility surface object
    Run method to compute volatility surface fir a specific ticker

--- Prototype/instrument.py
+++ Prototype/instrument.py
@@ -1,5 +1,7 @@
 from loguru import logger
 import os 
+from datetime import datetime, timedelta
+from typing import Union
 
 LIST_SYMBOL = ['F','G','H','J','K','M','N','Q','U','V','X','Z']
 
@@ -14,3 +16,34 @@ def create_folder(path : str):
 	if not os.path.exists(path):
 		logger.info(f'creation folder {path}')
 		os.makedirs(path)
+
+def business_date(date : datetime = datetime.now()):
+	#check if Saturday
+	if date.strftime("%w") == '6':
+		shift = timedelta(days = 1)
+	#check if Sunday
+	if date.strftime("%w") == '0':
+		shift = timedelta(days = 2)
+	cob = date - shift
+	return datetime(cob.year,cob.month,cob.day)
+
+def expiration_in_year(date1 : datetime,
+					   date2 : datetime,
+					   formatting :str = '%d-%m-%Y',
+					   convention : str = 'trading_days' ) -> float :
+	
+	date1 = datetime.strptime(date1, formatting)
+	date2 = datetime.strptime(date2, formatting)
+	days = (date2 - date1).days
+	if convention == 'actual':
+		y = days/360
+	if convention == 'trading_days':
+		y = days/252
+
+	return y
+
+def chang_date_formatting(cob : Union[datetime,str] , formatting_from :str , formatting_to : str) -> str:
+	if isinstance(cob, str):
+		cob = datetime.strptime(cob,formatting_from)
+	cob = cob.strftime(formatting_to)
+	return cob
